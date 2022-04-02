d0622425b9d7e0ca238c2084af58a8b07e0cd0b2
Minghan <1115142293@qq.com>
2022-04-02 16:53:13 +0800

    Add Main.java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import view.BookingNumberCheckIn;
+
+/**
+ * This is the entrance of this kiosk.
+ *
+ * @author Minghan Li
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        new BookingNumberCheckIn();
+    }
+}
