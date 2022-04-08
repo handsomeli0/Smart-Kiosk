14185be633d735ec8c5ea334a0efe988d38f5bce
CXT <1146492540@qq.com>
2022-04-08 21:40:17 +0800

    Add the summary UI into the project.

--- src/view/Seat.java
+++ src/view/Seat.java
@@ -1,6 +1,7 @@
 package view;
 
 import controller.DataController;
+import model.Booking;
 import model.Flight;
 import java.io.*;
 import javax.swing.*;
@@ -9,9 +10,9 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class Seat {
     int selected=0,bus=0,mov=0,leg=0,sum,cha=-1;
-    int[] seatnum=new int[32];
 
     JFrame frame=new JFrame("Choosing Seat"); //创建Frame窗口
     JFrame frame1=new JFrame("Pay For");
@@ -35,12 +36,16 @@ public class Seat {
     JLabel im=new JLabel(image);
     ImageIcon plane=new ImageIcon("src/images/plane.png");
     JLabel pl=new JLabel(plane);
+    ImageIcon logo=new ImageIcon("src/images/brand.jpg");
+    JLabel lo=new JLabel(logo);
 
     JButton[] x=new JButton[100];
     JButton con=new JButton("Confirm");
     JButton con1=new JButton("Confirm");
+    JButton back2=new JButton("Back");
+    JButton back3=new JButton("Back to main");
 
-    public int formpay(int bus,int mov,int leg,String seatnumID){
+    public int formpay(int bus,int mov,int leg,String flightID,String passengerID, Booking A,int cha){
         JLabel id=new JLabel("Credit Card ID:");
         JTextField ID=new JTextField(20);
 
@@ -57,23 +62,64 @@ public class Seat {
         id.setBounds(360,275,300,40);
         ID.setBounds(500,280,300,30);
 
-        im.setBounds(0,0,1200,675);
-        jp2.add(im);
-
         con1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent actionEvent) {
-                int Id= Integer.parseInt(ID.getText().toString());
-                if(DataController.checkPayment(seatnumID,Id)) {
+                try{
+                    if("".equals(ID.getText())){
+                        JOptionPane.showMessageDialog(frame, "Please enter your credit card information. ","Missing information", JOptionPane.ERROR_MESSAGE);
+                    }
+                    else{
+                        int Id= Integer.parseInt(ID.getText().toString());
+                        if(DataController.checkPayment(passengerID,Id)) {
+                            DataController.updateSeat(flightID, cha);
+                            closeThis();
+                            new Meal(flightID,passengerID,A);
+                        }
+                        else{
+                            JOptionPane.showMessageDialog(frame, "The credit card information you entered is incorrect. ","Missing information", JOptionPane.ERROR_MESSAGE);
+                        }
+                    }
+                }
+                catch (Exception e) {
+                    System.out.println("Exception occoured : " + e);
+                }
+            }
+        });
+        back2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                try{
+                    new Seat(flightID,passengerID,A);
                     closeThis();
-                    new Meal();
                 }
-                else{
-                    JOptionPane.showMessageDialog(frame, "The credit card information you entered is incorrect ","Missing information", JOptionPane.ERROR_MESSAGE);
+                catch (Exception e) {
+                    System.out.println("Exception occoured : " + e);
                 }
+            }
+        });
+        back3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                try{
+                    new BookingNumberCheckIn();
+                    closeThis();
+                }
+                catch (Exception e) {
+                    System.out.println("Exception occoured : " + e);
+                }
+
             }
         });
         con1.setBounds(540, 600, 120, 30);
         jp2.add(con1);
+
+        back2.setBounds(200,600,120,30);
+        back3.setBounds(900,600,120,30);
+        jp2.add(back2);
+        jp2.add(back3);
+
+        lo.setBounds(820,10,350,67);
+        jp2.add(lo);
+        im.setBounds(0,0,1200,675);
+        jp2.add(im);
 
         frame1.add(jp2);
         frame1.setBounds(0, 0, 1200, 675);
@@ -82,8 +128,10 @@ public class Seat {
         return 0;
     }
 
-    public int form(String flightID,String seatnumID) {
+    public int form(String flightID,String passengerID, Booking A) {
         jp.setLayout(null);
+        JButton back=new JButton("Back");
+        JButton back1=new JButton("Back to main");
         boolean[] seatnum= DataController.getSeatsByFlightID(flightID);
         for(int i=0;i<32;i++)
         {
@@ -153,10 +201,10 @@ public class Seat {
         x[1].addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
-                    if (   seatnum[1] != true) {
-                            if(cha!=-1){
-                                x[cha].setBackground(Color.WHITE);
-                            }
+                    if ( seatnum[1] != true) {
+                        if(cha!=-1){
+                            x[cha].setBackground(Color.WHITE);
+                        }
                         JButton x = Seat.this.x[1];
                         x.setBackground(Color.RED);
                         selected = 1;
@@ -176,13 +224,13 @@ public class Seat {
                         if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
-                    selected = 1;
-                    JButton x = Seat.this.x[2];
-                    x.setBackground(Color.RED);
-                    bus = 1;
-                    cha = 2;
+                        selected = 1;
+                        JButton x = Seat.this.x[2];
+                        x.setBackground(Color.RED);
+                        bus = 1;
+                        cha = 2;
+                    }
                 }
-            }
                 catch (Exception e) {
                     System.out.println("Exception occoured : " + e);
                 }
@@ -192,7 +240,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[3] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -211,7 +259,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[4] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -230,7 +278,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[5] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -249,7 +297,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[6] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -268,7 +316,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[7] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -286,8 +334,8 @@ public class Seat {
         x[8].addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
-                    if (   seatnum[8] != true) {
-                          if(cha!=-1){
+                    if (  seatnum[8] != true) {
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -305,7 +353,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[9] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -323,7 +371,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[10] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -341,7 +389,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[11] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -359,7 +407,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[12] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -377,7 +425,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[13] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -395,7 +443,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[14] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -413,7 +461,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[15] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -431,7 +479,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[16] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -449,7 +497,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[17] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -467,7 +515,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[18] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -485,7 +533,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[19] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -503,7 +551,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[20] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -521,7 +569,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[21] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -539,7 +587,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[22] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -558,7 +606,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[23] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -576,7 +624,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[24] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -594,7 +642,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[25] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -612,7 +660,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (seatnum[26] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -630,7 +678,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (seatnum[27] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -648,7 +696,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[28] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -666,7 +714,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[29] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -684,7 +732,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (   seatnum[30] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -702,7 +750,7 @@ public class Seat {
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     if (seatnum[31] != true) {
-                          if(cha!=-1){
+                        if(cha!=-1){
                             x[cha].setBackground(Color.WHITE);
                         }
                         selected = 1;
@@ -745,11 +793,10 @@ public class Seat {
                         if (bus == 0 && mov == 0 && leg == 0) {
                             DataController.updateSeat(flightID, cha);
                             closeThis();
-                            new Meal();
+                            new Meal(flightID,passengerID,A);
                         } else {
-                            DataController.updateSeat(flightID, cha);
                             closeThis();
-                            formpay(bus, mov, leg, seatnumID);
+                            formpay(bus, mov, leg, flightID,passengerID,A,cha);
                         }
                     }
                     else{
@@ -761,14 +808,31 @@ public class Seat {
                 }
             }
         });
+        back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                new Summary(A);
+                closeThis();
+            }
+        });
+        back1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                new BookingNumberCheckIn();
+                closeThis();
+            }
+        });
+
 
         con.setBounds(520, 600, 120, 30);
         jp.add(con);
+        back.setBounds(200,600,120,30);
+        back1.setBounds(800,600,120,30);
+        jp.add(back);
+        jp.add(back1);
 
         lable.setFont(font1);
         lable.setForeground(Color.white);
         jp.add(lable);
-        lable.setBounds(150,0,900,100);
+        lable.setBounds(0,0,850,100);
         lable2.setFont(font3);
         lable2.setForeground(Color.BLACK);
         jp.add(lable2);
@@ -783,6 +847,8 @@ public class Seat {
         fm.setBounds(680,450,100,20);
         jp.add(fm);
 
+        lo.setBounds(820,10,350,67);
+        jp.add(lo);
         im.setBounds(0,0,1200,675);
         jp.add(pl);
         pl.setBounds(150,150,900,300);
@@ -800,9 +866,8 @@ public class Seat {
         frame.dispose();
         frame1.dispose();
     }
-    public Seat(String flightID,String seatnumID)
+    public Seat(String flightID, String passengerID, Booking A)
     {
-        form(flightID,seatnumID);
+        form(flightID,passengerID,A);
     }
 }
-
