c5b9c79130f9b581f81c1790fafe96e355dae771
Minghan <1115142293@qq.com>
2022-05-13 01:04:26 +0800

    Add sendInformation in PeripheralController.java and writeCSV in FileIOController.java

--- src/controller/PeripheralController.java
+++ src/controller/PeripheralController.java
@@ -7,7 +7,7 @@ import java.io.IOException;
  * This class is used to manage peripherals.
  *
  * @author Minghan Li
- * @version 1.0
+ * @version 2.0
  */
 
 public class PeripheralController {
@@ -17,7 +17,7 @@ public class PeripheralController {
      *
      * @param bk booking information
      * @param classType class type
-     * @param seat set number
+     * @param seat seat number
      * @throws IOException IOException
      */
     public static void printBoardingPass(Booking bk, int classType, int seat) throws IOException {
@@ -67,5 +67,24 @@ public class PeripheralController {
         FileIOController.writeImage("Ticket", strs);
         JOptionPane.showMessageDialog(null, "Baggage Ticket has been printed! Please find it in the folder \"peripherals\".");
 
+    }
+
+    /**
+     * Send check-in information to the back-end system(/Smart-Kiosk/peripherals/Check-In Information.csv)
+     * @param bk booking information
+     * @param seatlevel class of the seat
+     * @param seatNum seat number
+     * @param meal meal name
+     * @param gourmet gourmet food name
+     * @param price price of chosen extra options
+     * @throws IOException IOException
+     */
+    public static void sendInformation(Booking bk, int seatlevel, int seatNum, String meal, String gourmet, double price) throws IOException {
+        String classType = seatlevel == 0 ? "Economy" : "Business";
+        String str = bk.getBookingNum() +","+ bk.getPassengerID() +","+ DataController.getNameByBooking(bk)+","+
+                DataController.getPassByBooking(bk).getIdNum()+","+DataController.getPassByBooking(bk).getCreditCardID()
+                +","+bk.getFlightID()+","+classType+","+seatNum+","+meal+","+gourmet+","+bk.getTagNum()+","
+                +bk.getTicketNum()+","+bk.getBagDropCounterNum()+","+price;
+        FileIOController.writeCSV("Check-In Information", str);
     }
 }
